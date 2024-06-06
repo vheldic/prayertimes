2de4815dea4a374308a66e93419aaa812950c165
vheldic <velid2004@gmail.com>
2024-06-06 17:06:04 +0200

    changed the visualisation of single-digited date

--- src/api/getData.js
+++ src/api/getData.js
@@ -21,7 +21,8 @@ export async function getLocations() {
 function formatDate(str) {
     const monthNum = str.slice(str.indexOf("-") + 1, str.lastIndexOf("-"))
     const monthStr = data.months[monthNum - 1]
-    const day = str.slice(str.lastIndexOf("-") + 1, str.length);
+    let day = str.slice(str.lastIndexOf("-") + 1, str.length);
+    if (day.substring(0, 1) === "0") day = day.substring(1, day.length)
     const year = str.slice(0, str.indexOf("-"));
     const formatedDate = day + ". " + monthStr + " " + year;
     return formatedDate;
